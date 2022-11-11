8db2e66620ae84f589b96208e6eb8a4a1c807b17
Andrew Hunter <iamhunter@users.noreply.github.com>
2022-11-10 22:43:28 -0500

    tutorial (#1)
    * tutorial
    
    * fix ts errors
    
    * more ts

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,42 @@
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
+
+export async function getStaticProps({ params }: { params: { id: number } }) {
+    // Add the "await" keyword like this:
+    const postData = await getPostData(params.id);
+
+    return {
+        props: {
+            postData,
+        },
+    };
+}
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
+export default function Post({ postData }: any) {
+    return (
+        <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
+        </Layout>
+    );
+}
