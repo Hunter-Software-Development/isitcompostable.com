6cbe345597f7c9fbee2360262ffeef8e441b202b
Andrew Hunter <iamhunter@users.noreply.github.com>
2022-12-05 18:05:21 -0500

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,24 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
             <Component {...pageProps} />
             <Analytics />
+            <div>
+                <Script src="https://www.googletagmanager.com/gtag/js?id=G-0FZWLWCNYW" strategy="afterInteractive" />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){window.dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-0FZWLWCNYW');
+                    `}
+                </Script>
+            </div>
         </>
     );
 }
