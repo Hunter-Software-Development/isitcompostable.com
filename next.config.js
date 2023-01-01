9426fe3312843d8ca8794d2c1a6a90ee40221fb9
Andrew Hunter <iamhunter@users.noreply.github.com>
2023-01-01 16:14:01 -0500

    pwa

--- next.config.js
+++ next.config.js
@@ -1,49 +1,55 @@
 /** @type {import('next').NextConfig} */
 const { withPlaiceholder } = require("@plaiceholder/next");
 
-module.exports = withPlaiceholder({
-    reactStrictMode: true,
-    swcMinify: true,
-    webpack: (config) => {
-        config.resolve = {
-            ...config.resolve,
-            fallback: {
-                fs: false,
-                path: false,
-                os: false,
-            },
-        };
-        return config;
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "www.publicdomainpictures.net",
-            },
-            {
-                protocol: "https",
-                hostname: "**.wikimedia.org",
-            },
-            {
-                protocol: "https",
-                hostname: "openailabsprodscus.blob.core.windows.net",
-            },
-            {
-                protocol: "https",
-                hostname: "isitcompostable.com",
-            },
-            {
-                protocol: "https",
-                hostname: "cdn.shopify.com",
-            },
-        ],
-    },
-    experimental: {
-        // This is experimental but can
-        // be enabled to allow parallel threads
-        // with nextjs automatic static generation
-        workerThreads: false,
-        cpus: 1,
-    },
+const withPWA = require("next-pwa")({
+    dest: "public",
 });
+
+module.exports = withPWA(
+    withPlaiceholder({
+        reactStrictMode: true,
+        swcMinify: true,
+        webpack: (config) => {
+            config.resolve = {
+                ...config.resolve,
+                fallback: {
+                    fs: false,
+                    path: false,
+                    os: false,
+                },
+            };
+            return config;
+        },
+        images: {
+            remotePatterns: [
+                {
+                    protocol: "https",
+                    hostname: "www.publicdomainpictures.net",
+                },
+                {
+                    protocol: "https",
+                    hostname: "**.wikimedia.org",
+                },
+                {
+                    protocol: "https",
+                    hostname: "openailabsprodscus.blob.core.windows.net",
+                },
+                {
+                    protocol: "https",
+                    hostname: "isitcompostable.com",
+                },
+                {
+                    protocol: "https",
+                    hostname: "cdn.shopify.com",
+                },
+            ],
+        },
+        experimental: {
+            // This is experimental but can
+            // be enabled to allow parallel threads
+            // with nextjs automatic static generation
+            workerThreads: false,
+            cpus: 1,
+        },
+    })
+);
