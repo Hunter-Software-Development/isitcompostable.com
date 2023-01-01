d36fa525791e586b3b3980176e7bc3713fc02a89
Andrew Hunter <iamhunter@users.noreply.github.com>
2023-01-01 17:40:53 -0500

    pwa (#13)
    * pwa
    
    * tweak manifest
    
    * optimize build

--- pages/_document.js
+++ pages/_document.js
@@ -29,6 +29,20 @@ class MyDocument extends Document {
                     {this.props.stylesheets.map((sheet, i) => (
                         <style className="_styletron_hydrate_" dangerouslySetInnerHTML={{ __html: sheet.css }} media={sheet.attrs.media} data-hydrate={sheet.attrs["data-hydrate"]} key={i} />
                     ))}
+
+                    {/* PWA Config */}
+                    <meta name="application-name" content="PWA App" />
+                    <meta name="apple-mobile-web-app-capable" content="yes" />
+                    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+                    <meta name="apple-mobile-web-app-title" content="PWA App" />
+                    <meta name="description" content="Best PWA App in the world" />
+                    <meta name="format-detection" content="telephone=no" />
+                    <meta name="mobile-web-app-capable" content="yes" />
+                    <meta name="msapplication-TileColor" content="#2B5797" />
+                    <meta name="msapplication-tap-highlight" content="no" />
+                    <meta name="theme-color" content="#000000" />
+                    <link rel="manifest" href="/manifest.json" />
+                    <link rel="shortcut icon" href="/favicon.ico" />
                 </Head>
                 <body>
                     <Main />
